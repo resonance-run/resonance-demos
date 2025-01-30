04efceef7d8889342a0ae4510e8ecd475b69f5ca
Steven Foote <steven@resonance.run>
2025-01-30 15:06:07 -0700

    Upgrade to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { reactRouter } from '@react-router/dev/vite';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     cssMinify: true,
   },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 }));
