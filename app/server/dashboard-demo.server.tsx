96615c9bb1fcdbab30a86f02cd1228bc38276138
Steven Foote <steven@resonance.run>
2025-02-27 12:36:30 -0700

    Allow the dashboard demo to be personalized

--- app/server/dashboard-demo.server.tsx
+++ app/server/dashboard-demo.server.tsx
@@ -1,4 +1,5 @@
 import { IconName } from '~/components/Icon';
+import { getResonanceInstance } from './resonance-sdk.server';
 
 interface NavSectionLink {
   name: string;
@@ -12,42 +13,76 @@ interface NavSection {
   links: NavSectionLink[];
 }
 
-export const getNavSections = (userData: Record<string, unknown>, request: Request): NavSection[] => {
-  return [
-    {
-      id: 'main',
-      links: [
-        { name: 'Search', to: '/dashboard-demo', icon: IconName.search },
-        { name: 'Home', to: '/dashboard-demo', icon: IconName.home },
-        { name: 'Inbox', to: '/dashboard-demo', icon: IconName.inbox },
-      ],
-    },
-    {
-      id: 'private',
-      title: 'Private',
-      links: [
-        { name: 'Getting started', to: '/dashboard-demo', icon: IconName.pageWithWriting },
-        { name: 'To Do List', to: '/dashboard-demo', icon: IconName.checkSquare },
-        { name: 'Travel Planner', to: '/dashboard-demo', icon: IconName.plane },
-      ],
-    },
-    {
-      id: 'team',
-      title: 'Teams',
-      links: [
-        { name: 'Direction', to: '/dashboard-demo', icon: IconName.compass },
-        { name: 'Add new', to: '/dashboard-demo', icon: IconName.plus },
-      ],
-    },
-    {
-      id: 'admin',
-      links: [
-        { name: 'Settings', to: '/dashboard-demo', icon: IconName.settings },
-        { name: 'Templates', to: '/dashboard-demo', icon: IconName.template },
-        { name: 'Trash', to: '/dashboard-demo', icon: IconName.trash },
-      ],
-    },
-  ];
+export const getNavSections = async (userData: Record<string, unknown>, request: Request): Promise<NavSection[]> => {
+  const resonance = getResonanceInstance();
+  const mainDefault: NavSection = {
+    id: 'main',
+    links: [
+      { name: 'Search', to: '/dashboard-demo', icon: IconName.search },
+      { name: 'Home', to: '/dashboard-demo', icon: IconName.home },
+      { name: 'Inbox', to: '/dashboard-demo', icon: IconName.inbox },
+    ],
+  };
+  const main: NavSection = await resonance.loadCustomization({
+    customizationType: 'demo-navigation-section',
+    surfaceId: 'main',
+    userData,
+    request,
+    defaultValue: mainDefault,
+  });
+
+  const privateDefault: NavSection = {
+    id: 'private',
+    title: 'Private',
+    links: [
+      { name: 'Getting started', to: '/dashboard-demo', icon: IconName.pageWithWriting },
+      { name: 'To Do List', to: '/dashboard-demo', icon: IconName.checkSquare },
+      { name: 'Travel Planner', to: '/dashboard-demo', icon: IconName.plane },
+    ],
+  };
+  const privateSection: NavSection = await resonance.loadCustomization({
+    customizationType: 'demo-navigation-section',
+    surfaceId: 'private',
+    userData,
+    request,
+    defaultValue: privateDefault,
+  });
+
+  const teamsDefault: NavSection = {
+    id: 'team',
+    title: 'Teams',
+    links: [
+      { name: 'Direction', to: '/dashboard-demo', icon: IconName.compass },
+      { name: 'Add new', to: '/dashboard-demo', icon: IconName.plus },
+    ],
+  };
+
+  const teamsSection: NavSection = await resonance.loadCustomization({
+    customizationType: 'demo-navigation-section',
+    surfaceId: 'teams',
+    userData,
+    request,
+    defaultValue: teamsDefault,
+  });
+
+  const adminDefault: NavSection = {
+    id: 'admin',
+    links: [
+      { name: 'Settings', to: '/dashboard-demo', icon: IconName.settings },
+      { name: 'Templates', to: '/dashboard-demo', icon: IconName.template },
+      { name: 'Trash', to: '/dashboard-demo', icon: IconName.trash },
+    ],
+  };
+
+  const admin: NavSection = await resonance.loadCustomization({
+    customizationType: 'demo-navigation-section',
+    surfaceId: 'admin',
+    userData,
+    request,
+    defaultValue: adminDefault,
+  });
+
+  return [main, privateSection, teamsSection, admin];
 };
 
 export const getDashboardModules = (userData: Record<string, unknown>, request: Request) => {
